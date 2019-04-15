efd18ae618c333b6d2b17b0f52851b484a1f7ac0
Djurica Djuricic <djurica.djuricic@seavus.com>
2019-04-15 13:17:28 +0200

    Basic widgets & Assets folder.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(RootWidget());
-}
+void main() => runApp(MyApp());
 
-class RootWidget extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("App Title"),
+        ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset("assets/image.jpg"),
+              Text("Card text."),
+            ],
+          ),
+          color: Colors.amberAccent,
         ),
       ),
     );
