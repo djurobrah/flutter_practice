c50d6840bca1dc5337d05b00787f71b6df1387f2
Djurica Djuricic <djurica.djuricic@seavus.com>
2019-04-17 14:18:20 +0200

    Column alignment.

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 
-class ProductPage extends StatelessWidget
-{
-	@override
-	Widget build(BuildContext context)
-	{
-		return Scaffold(
-			appBar: AppBar(
-				title: Text("Product Detail"),
-			),
-			body: Column(
-				children: <Widget>[
-					Center(child: Text("Product details"),),
-					Center(
-						child: RaisedButton(child: Text("Back"), onPressed: ()
-						=> Navigator.pop(context)))
-				],
-			),
-		);
-	}
+class ProductPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Product Detail"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+//            Image.asset('assets/image.jpg'),
+            Text("Product details"),
+            RaisedButton(
+                child: Text("Back"), onPressed: () => Navigator.pop(context))
+          ],
+        ),
+      ),
+    );
+  }
 }
