45a56175654af587f57a2476c52189a7d74f8640
Djurica Djuricic <djurica.djuricic@seavus.com>
2019-04-17 13:45:58 +0200

    Back navigation via pop.

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
-class ProductPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Product Detail"),
-      ),
-      body: Center(child: Text("Product details")),
-    );
-  }
+class ProductPage extends StatelessWidget
+{
+	@override
+	Widget build(BuildContext context)
+	{
+		return Scaffold(
+			appBar: AppBar(
+				title: Text("Product Detail"),
+			),
+			body: Column(
+				children: <Widget>[
+					Center(child: Text("Product details"),),
+					Center(
+						child: RaisedButton(child: Text("Back"), onPressed: ()
+						=> Navigator.pop(context)))
+				],
+			),
+		);
+	}
 }
