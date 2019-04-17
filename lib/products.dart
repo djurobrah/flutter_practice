69d5d7a4393c30b679ff9edb47bd2909ca17eb81
Djurica Djuricic <djurica.djuricic@seavus.com>
2019-04-17 10:41:19 +0200

    ListView upgrade (kao RecyclerView nekada).

--- lib/products.dart
+++ lib/products.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 
-class Products extends StatelessWidget {
-  final List<String> products;
-
-  Products(this.products);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: products
-          .map((element) => Card(
-                color: Colors.amberAccent,
-                child: Column(
-                  children: <Widget>[
-                    Image.asset("assets/image.jpg"),
-                    Text(element),
-                  ],
-                ),
-              ))
-          .toList(),
-    );
-  }
+class Products extends StatelessWidget
+{
+	final List<String> products;
+	
+	Products(this.products);
+	
+	Widget _buildProductItem(BuildContext context, int index)
+	{
+		return Card(
+			color: Colors.amberAccent,
+			child: Column(
+				children: <Widget>[
+					Image.asset("assets/image.jpg"),
+					Text(products[index]),
+				],
+			),
+		);
+	}
+	
+	@override
+	Widget build(BuildContext context)
+	{
+		return ListView.builder(
+			itemBuilder: _buildProductItem,
+			itemCount: products.length,
+		);
+	}
 }
