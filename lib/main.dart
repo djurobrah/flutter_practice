4345103dc201b17572f78996ca7c40604105d8af
Djurica Djuricic <djurica.djuricic@seavus.com>
2019-04-23 13:11:37 +0200

    onUnknownRoute (fallback route)

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,11 @@ class _MyAppState extends State<MyApp> {
         }
         return null;
       },
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+            builder: (BuildContext context) =>
+                ProductsPage(_products, _addProducts, _deleteProduct));
+      },
     );
   }
 }
