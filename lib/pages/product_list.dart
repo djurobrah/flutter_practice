ad2180d07f559a78735228d498e195c536eef384
Djurica Djuricic <djurica.djuricic@seavus.com>
2019-05-17 11:58:41 +0200

    Using Dismissible to delete product.

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -4,40 +4,50 @@ import 'package:flutter_practice/pages/product_edit.dart';
 class ProductListPage extends StatelessWidget {
   final List<Map<String, dynamic>> products;
   final Function updateProduct;
+  final Function deleteProduct;
 
-  ProductListPage(this.products, this.updateProduct);
+  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: products.length,
       itemBuilder: (BuildContext context, int index) {
-        return Column(
-          children: <Widget>[
-            ListTile(
-              leading: CircleAvatar(
-                  backgroundImage: AssetImage(products[index]['image'])),
-              title: Text(products[index]['title']),
-              subtitle: Text("\$" + products[index]['price'].toString()),
-              trailing: IconButton(
-                icon: Icon(Icons.edit),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) {
-                        return ProductEditPage(
-                          product: products[index],
-                          updateProduct: updateProduct,
-                          productIndex: index,
-                        );
-                      },
-                    ),
-                  );
-                },
+        return Dismissible(
+          key: Key(products[index]['title']),
+          background: Container(
+            color: Colors.red,
+          ),
+          onDismissed: (DismissDirection direction) {
+            deleteProduct(index);
+          },
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                    backgroundImage: AssetImage(products[index]['image'])),
+                title: Text(products[index]['title']),
+                subtitle: Text("\$" + products[index]['price'].toString()),
+                trailing: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return ProductEditPage(
+                            product: products[index],
+                            updateProduct: updateProduct,
+                            productIndex: index,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-            Divider(),
-          ],
+              Divider(),
+            ],
+          ),
         );
       },
     );
