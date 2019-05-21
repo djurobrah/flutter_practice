8cc11f3f81bb248b51be8a16ddc7e4bc132b973b
Djurica Djuricic <djurica.djuricic@seavus.com>
2019-05-21 09:42:55 +0200

    Providing a single ScopedModel instance.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter_practice/pages/auth.dart';
 import 'package:flutter_practice/pages/product.dart';
 import 'package:flutter_practice/pages/products.dart';
 import 'package:flutter_practice/pages/products_admin.dart';
+import 'package:flutter_practice/scoped-models/products.dart';
+
+import 'package:scoped_model/scoped_model.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,34 +18,38 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-        accentColor: Colors.deepPurple,
-        fontFamily: 'Montserrat',
-        buttonColor: Colors.deepPurple,
-      ),
-      routes: {
-        '/': (BuildContext context) => AuthPage(),
-        '/products': (BuildContext context) => ProductsPage(),
-        '/admin': (BuildContext context) => ProductAdminPage(),
-      },
-      onGenerateRoute: (RouteSettings settings) {
-        final List<String> pathElements = settings.name.split("/");
-        if (pathElements[0] != "") {
-          return null;
-        }
-        if (pathElements[1] == "product") {
+    return ScopedModel<ProductsModel>(
+      model: ProductsModel(),
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple,
+          fontFamily: 'Montserrat',
+          buttonColor: Colors.deepPurple,
+        ),
+        routes: {
+          '/': (BuildContext context) => AuthPage(),
+          '/products': (BuildContext context) => ProductsPage(),
+          '/admin': (BuildContext context) => ProductAdminPage(),
+        },
+        onGenerateRoute: (RouteSettings settings) {
+          final List<String> pathElements = settings.name.split("/");
+          if (pathElements[0] != "") {
+            return null;
+          }
+          if (pathElements[1] == "product") {
 //          final int index = int.parse(pathElements[2]);
-          return MaterialPageRoute<bool>(
-              builder: (BuildContext context) => ProductPage(null, null, null, null));
-        }
-        return null;
-      },
-      onUnknownRoute: (RouteSettings settings) {
-        return MaterialPageRoute(
-            builder: (BuildContext context) => ProductsPage());
-      },
+            return MaterialPageRoute<bool>(
+                builder: (BuildContext context) =>
+                    ProductPage(null, null, null, null));
+          }
+          return null;
+        },
+        onUnknownRoute: (RouteSettings settings) {
+          return MaterialPageRoute(
+              builder: (BuildContext context) => ProductsPage());
+        },
+      ),
     );
   }
 }
